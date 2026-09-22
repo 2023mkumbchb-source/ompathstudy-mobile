6b38817998e293d54e2e521969b1d44b81818240
2023mkumbchb-source <2023mkumbchb@gmail.com>
2026-09-22 10:18:51 +0300

    refine: polish reading and source surfaces

--- src/pages/SourceLibrary.tsx
+++ src/pages/SourceLibrary.tsx
@@ -1,3 +1,4 @@
+import "@/styles/workspace.css";
 import { useEffect, useMemo, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { Check, Clipboard, Download, FileQuestion, Loader2, Save, Search } from "lucide-react";
